3e5a7d6cc200c44013f00e43493b06ac5ec82530
quangvu0407 <trantuyenquang0407@gmail.com>
2026-04-02 21:14:27 +0700

    config fe

---
+++ nextjs_ad/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button type="primary">
+        Add new
+      </Button>
+    </div>
+  );
+}
